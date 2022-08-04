5540aa2a6d5280a13e11b662c11f4d6c55b6ead9
absdevelop <abnergmtomaz@gmail.com>
2022-08-04 09:33:34 -0300

    Design

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { React } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>
+        Previsão do Tempo
+      </Text>
+
+      <Image style={styles.img}
+        source={{uri: 'https://cdn-icons-png.flaticon.com/512/2849/2849457.png',}}  
+      />
+
+      <Text style={styles.textBlock}>Informação sua Cidade:</Text>
+      <TextInput style={styles.input}
+        placeholder='Sua cidade...'
+      />
+        
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.textbtn}>Buscar</Text>
+      </TouchableOpacity>	          
     </View>
   );
 }
@@ -14,7 +28,41 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 8,
+    alignItems: 'center'
   },
+  title: {
+    backgroundColor: 'black',
+    color: 'white',
+    fontSize: 30,
+    justifyContent: 'center',
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+	  marginTop: 60
+  },	
+  img: {
+    width: 190,
+    height: 190,
+    marginTop: 30,
+    marginBottom: 30
+  },
+  input: {
+    backgroundColor: 'white'
+  },
+  textBlock: {
+    fontSize: 20,
+	  justifyContent: 'center'
+  },
+  btn: {
+	  width: '80%',
+  	marginTop: 10,
+  	marginBottom: 15,
+    borderWidth: 1,
+    borderRadius: 12,
+    borderBottomColor: 'black'
+  },
+  textbtn: {
+	  fontSize: 20,
+	  textAlign: 'center'
+  }
 });
