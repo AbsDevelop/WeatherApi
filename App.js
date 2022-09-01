417208c9fb980b0a95eda0a31329f63d76cd7ff4
absdevelop <abnergmtomaz@gmail.com>
2022-09-01 08:44:06 -0300

    Ready to use

--- App.js
+++ App.js
@@ -1,45 +1,54 @@
 import React, {useState}  from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, ScrollView } from 'react-native';
 import Tempo from './components/Tempo';
 import Api from './components/Api';
 
 export default function App() {
   const [dadosA, setDadosA] = useState("");
   const [dadosB, setDadosB] = useState("");
+  const [dadosAS, setDadosAS] = useState("");
+  const [dadosBS, setDadosBS] = useState("");
   const [cidade, setCidade] = useState("");
 
   async function buscaCep(){
-    const response = await Api.get(`weather?array_limit=2&fields=only_results,temp,city_name,forecast,max,min,date,description&key=ca53326e&city_name=${cidade}`);
+    const response = await Api.get(`weather?array_limit=2&fields=only_results,temp,city_name,forecast,max,min,date,time,description,city,humidity,wind_speedy%20&key=ca53326e&city_name=${cidade}`);
     setDadosA(response.data.forecast[0]);
     setDadosB(response.data.forecast[0]);
+    setDadosAS(response.data);
+    setDadosBS(response.data);
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        Previsão do Tempo
-      </Text>
+    <ScrollView style={styles.scrollView}>
+      <View style={styles.container}>
+          <Text style={styles.title}>
+            Previsão do Tempo
+          </Text>
 
-      <Image style={styles.img}
-        source={{uri: 'https://eusousolar.com.br/calculadora/img/sun.gif',}}  
+          <Image style={styles.img}
+            source={{uri: 'https://eusousolar.com.br/calculadora/img/sun.gif',}}  
 
-        //https://dribbble.com/shots/6193517-Weather-Icon-Set-Thunderstorm
-        //https://github.com/Ema Suriano/weather-styled-icon
-      />
+            //https://dribbble.com/shots/6193517-Weather-Icon-Set-Thunderstorm
+            //https://github.com/Ema Suriano/weather-styled-icon
+          />
 
-      <Text style={styles.textBlock}>Informe sua Cidade:</Text>
-      <TextInput style={styles.input}
-        placeholder='                Ex.: São Paulo, SP                '
-        onChangeText={data=>setCidade(data)}
-      />
-        
-      <TouchableOpacity style={styles.btn} onPress={buscaCep}>
-        <Text style={styles.textbtn}>Buscar</Text>
-      </TouchableOpacity>	 
-      <Tempo dataA={dadosA}
-       dataB={dadosB} />
-
-    </View>
+          <Text style={styles.textBlock}>Informe sua Cidade:</Text>
+          <TextInput style={styles.input}
+            placeholder='                Ex.: São Paulo, SP                '
+            onChangeText={data=>setCidade(data)}
+          />
+            
+          <TouchableOpacity style={styles.btn} onPress={buscaCep}>
+            <Text style={styles.textbtn}>Buscar</Text>
+          </TouchableOpacity>	 
+          
+            <Tempo dataA={dadosA}
+            dataB={dadosB} 
+            dataAS={dadosAS}
+            dataBS={dadosBS} 
+            /> 
+      </View>
+    </ScrollView>
   );
 }
 
--- components/Tempo.js
+++ components/Tempo.js
@@ -2,19 +2,33 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 export default function Tempo(props) {
- return (
-   <>
-    <View style={styles.block}>
-          <Text style={styles.title}>Hoje:</Text>
-          <Text style={styles.text}>Máxima: {props.dataA.max}</Text>
-          <Text style=  {styles.text}>Mínima: {props.dataA.min}</Text>
+  const br = `\n`;
+  return (
+    <>
+      <View style={styles.block}>
+        <Text style={styles.title}>Cidade: </Text><Text style={styles.text}>{props.dataAS.city}{br}</Text>
+
+         <Text style={styles.title}>Hoje:</Text>
+          <Text style={styles.text}>Temperatura: {props.dataAS.temp}°C</Text>
+          <Text style={styles.text}>Máxima: {props.dataA.max}°C</Text>
+          <Text style=  {styles.text}>Mínima: {props.dataA.min}°C</Text>
           <Text style={styles.text}>Tempo: {props.dataA.description}</Text>
-          <Text style={styles.title}>Amanhã:</Text>
-          <Text style={styles.text}>Máxima: {props.dataB.max}</Text>
-          <Text style={styles.text}>Mínima: {props.dataB.min}</Text>
-          <Text style={styles.text}>Tempo: {props.dataB.description}</Text>   
-    </View>
-   </>
+          <Text style={styles.text}>Data: {props.dataA.date}</Text>
+          <Text style={styles.text}>Hora: {props.dataAS.time}h{br}</Text>
+
+        <Text style={styles.title}>Amanhã:</Text>
+          <Text style={styles.text}>Temperatura: {props.dataBS.temp}°C</Text>
+          <Text style={styles.text}>Máxima: {props.dataB.max}°C</Text>
+          <Text style={styles.text}>Mínima: {props.dataB.min}°C</Text>
+          <Text style={styles.text}>Tempo: {props.dataB.description}</Text> 
+          <Text style={styles.text}>Data: {props.dataB.date}</Text>
+          <Text style={styles.text}>Hora: {props.dataBS.time}h{br}</Text>  
+          
+        <Text style={styles.title}>Extras:</Text>  
+          <Text style={styles.text}>Umidade: {props.dataAS.humidity}%</Text>
+          <Text style={styles.text}>Velocidade do Vento: {props.dataAS.wind_speedy}</Text>
+      </View>
+    </>
   );
 }
 
@@ -38,17 +52,3 @@ const styles = StyleSheet.create({
       backgroundColor: 'white'
     }
 });
-
-/*    
-      Link: "weather?array_limit=1&fields=only_results,temp,city_name,forecast,max,min,date,time,description,city,humidity,wind_speedy%20&key=ca53326e&city_name="
-      Blocos:
-      <Text style={styles.text}>Temperatura: {props.dados2.temp}</Text>
-
-      <Text style={styles.text}>Data: {props.dados2.date}</Text>
-      <Text style={styles.text}>Hora: {props.dados2.time}</Text>
-      
-      <Text style={styles.text}>Cidade: {props.dados2.city}</Text>
-      <Text style={styles.text}>Umidade: {props.dados2.humidity}</Text>
-      <Text style={styles.text}>Velocidade do Vento: {props.dados2.wind_speedy}</Text>
-        
-*/
